1d1b0f8ed89c2c0ed7bfa090fe5255b0b5699e00
Happyure <satriya.pz@gmail.com>
2022-03-05 21:35:59 +0700

    amortisasi1

--- Amortisasi.py
+++ Amortisasi.py
@@ -1,1 +1,15 @@
-print('halo bang')
+
+jumlahpinjamanawal=int(input('Masukkan jumlah pinjaman awal : '))
+sukubunga=int(input('Masukkan suku bunga (persen) : '))/100
+tenorpengembalian=int(input('Masukkan tenor pengembalian : '))
+
+pembayaranperperiode=jumlahpinjamanawal*sukubunga*((1+sukubunga)**tenorpengembalian)//(((1+sukubunga)**tenorpengembalian)-1)
+bungaperiode=sisahutang*sukubunga
+pokokbayar=pembayaranperperiode-bungaperiode
+sisahutang=jumlahpinjamanawal-pokokbayar
+
+print('\nBiaya pokok : ',pokokbayar)
+print('Bunga periode : ',bungaperiode)
+print('Pembayaran perperiode : ',pembayaranperperiode)
+print('Sisa hutang : ',sisahutang)
+print('Total angsuran : ',pembayaranperperiode*3,'\n')

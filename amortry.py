1f25e911677c826522f62673ce6a82722cd19a81
Happyure <satriya.pz@gmail.com>
2022-03-16 22:10:14 +0700

    amortisasi

--- amortry.py
+++ amortry.py
@@ -74,9 +74,9 @@ while pengulangan==0:
             data_angsuran_periodik2[data2]=jumlah
             data_angsuran_periodik.update(data_angsuran_periodik2)
             data2+=1
+            data_angsuran_periodik2.clear()
             if data2>periode:
                 break
-            data_angsuran_periodik2.clear()
         while data2<=periode:
             data_angsuran_periodik[data2]=pembayaran_perperiode
             data2+=1

823f953bdcee14e3b5e9561aa46a91a70767e03f
Happyure <satriya.pz@gmail.com>
2022-04-08 20:57:46 +0700

    commit on 'master'

--- amortisasifix.py
+++ amortisasifix.py
@@ -4,8 +4,13 @@ data_angsuran_periodik={}
 data_angsuran_periodik2={}
 data_total_angsuran=[]
 banyak_periode=[]
-pilihan_periode=[]
 banyak_pengulangan=[]
+
+def error_yn():
+    try:
+        print('Mohon maaf, kami hanya menerima yes/no')
+    except:
+        return
 
 #Login
 userpass={'icljaya':'123','iclkeren':'456','iclmantap':'789'}
@@ -32,42 +37,38 @@ pengulangan=0
 while pengulangan==0:
 
     #Input
-    jumlah_pinjamanawal=int(input('\nMasukkan jumlah pinjaman awal\t: '))
-    suku_bunga=float(input('Masukkan suku bunga (persen)\t: '))/100
-    tenor_pengembalian=int(input('Masukkan tenor pengembalian\t: '))
+    while True:
+        try:
+            jumlah_pinjamanawal=int(input('\nMasukkan jumlah pinjaman awal\t   : '))
+            suku_bunga=float(input('Masukkan suku bunga (persen)\t   : '))/100
+            tenor_pengembalian=int(input('Masukkan tenor pengembalian (tahun): '))
+        except Exception:
+            print('Tolong masukkan angka dan bukan karakter')
+        else:
+            break
 
-    #Memilih Metode Pembayaran
-    print('\nBerikut ini adalah pilihan periode pembayaran:')
-    list_periode=['Tahunan','Tengah Tahunan','Kuartal','Bulanan']
-    nomor=1
-    for index in list_periode:
-        print(nomor,index)
-        nomor+=1
-    periode_terpilih=int(input('Masukkan pilihan periode pembayaran: '))
-    if periode_terpilih==1:
-        bunga=suku_bunga
-        periode=tenor_pengembalian
-    elif periode_terpilih==2:
-        bunga=suku_bunga/2
-        periode=tenor_pengembalian*2
-    elif periode_terpilih==3:
-        bunga=suku_bunga/4
-        periode=tenor_pengembalian*4
-    elif periode_terpilih==4:
-        bunga=suku_bunga/12
-        periode=tenor_pengembalian*12
-    else:
-        print("Pilihan tidak tersedia\n")
-        quit()
-    pilihan_periode.append(periode_terpilih)
-    periode_ke=1
-    while periode_ke<=periode:
-        banyak_periode.append(periode_ke)
-        periode_ke+=1
-
+    #pelunasan
+    while True:
+        pelunasan=input('\nApakah anda ingin melunasi pada waktu tertentu? (yes/no): ').lower
+        if pelunasan=='yes':
+            while True:
+                try:
+                    pelunasan_input=int(input('Masukkan waktu pelunasan (tahun): '))
+                    waktu_pelunasan=pelunasan_input*12
+                except Exception:
+                    print('Tolong masukkan angka dan bukan karakter')
+                else:
+                    break
+        elif pelunasan=='no':
+            break
+        else:
+            print('Tolong masukkan sesuai dengan pilihan yang tersedia')
+    
     #Menghitung Angsuran per Periode
+    bunga=suku_bunga/12
+    periode=tenor_pengembalian*12
     pembayaran_perperiode=jumlah_pinjamanawal*bunga*((1+bunga)**periode)//(((1+bunga)**periode)-1)
-    print('\nPembayaran perperiode\t: Rp.',int(pembayaran_perperiode))
+    print('\nPembayaran perbulan\t: Rp.',int(pembayaran_perperiode))  
     if data_angsuran_periodik=={}:
         data=1
         while data<=periode:
@@ -107,6 +108,7 @@ while pengulangan==0:
             print('Sisa hutang periode    ',periode_ke,': Rp.',int(sisa_hutang))
         jumlah_pinjamanawal=sisa_hutang
         periode_ke+=1
+
     mengulang=input('\nApakah anda ingin melakukan perhitungan peminjaman lainnya? (yes/no): ').lower()
     if mengulang=='no':
         pengulangan+=1
@@ -114,19 +116,18 @@ while pengulangan==0:
         banyak_pengulangan.append(1)
 
 if sum(banyak_pengulangan)>0:
-    if pilihan_periode[0]==pilihan_periode[1]:
-
-        #Membandingkan Efektivitas Perhitungan
-        perbandingan=input('\nApakah anda ingin mengetahui perbandingan pembayaran yang ada lakukan? (yes/no): ').lower()
-        if perbandingan=='yes':
-            print('\nPerhitungan ke-',data_total_angsuran.index(min(data_total_angsuran))+1,' lebih efektif karena total angsurannya lebih kecil yaitu Rp.',min(data_total_angsuran))
+    #Membandingkan Efektivitas Perhitungan
+    perbandingan=input('\nApakah anda ingin mengetahui perbandingan pembayaran yang ada lakukan? (yes/no): ').lower()
+    if perbandingan=='yes':
+        print('\nPerhitungan ke-',data_total_angsuran.index(min(data_total_angsuran))+1,' lebih efektif karena total angsurannya lebih kecil yaitu Rp.',min(data_total_angsuran))
         
-        #Menampilkan Angsuran Kumulatif per Periode
-        angsuran_kumulatif=input('\nApakah anda ingin menampilkan angsuran kumulatif tiap periode? (yes/no): ').lower()
-        if angsuran_kumulatif=='yes':
-            print('\nBerikut ini adalah angsuran kumulatif tiap periode: ')
-            for key,value in data_angsuran_periodik.items():
-                print('Angsuran Periode',key,': Rp.',value)  
+    #Menampilkan Angsuran Kumulatif per Periode
+    angsuran_kumulatif=input('\nApakah anda ingin menampilkan angsuran kumulatif tiap periode? (yes/no): ').lower()
+    if angsuran_kumulatif=='yes':
+        print('\nBerikut ini adalah angsuran kumulatif tiap periode: ')
+        for key,value in data_angsuran_periodik.items():
+            print('Angsuran Periode',key,': Rp.',value)
     print('\nBerikut ini adalah total angsuran semua pembayaran: Rp.',sum(data_total_angsuran))
 print('Terima kasih telah menggunakan program kami')
 print(userid,localtime,'\n')
+
